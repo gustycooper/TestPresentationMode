8629e9e106579e79f9e1b2351dbfe4c455e415ec
gustycooper <gusty.cooper@gmail.com>
2017-08-29 15:29:43 -0400

    Updated RoomTester

--- src/RoomTester.java
+++ src/RoomTester.java
@@ -3,6 +3,12 @@
  */
 public class RoomTester {
     public static void main(String[] args) {
+        if (args.length > 0)
+            System.out.println(args[0]);
+        if (args.length > 1)
+            System.out.println(args[1]);
+        if (args.length > 2)
+            System.out.println(args[2]);
         Room r = new Room("Trinkle", 6, 25);
         System.out.println(r.getBuilding() + " Room num: " + r.roomNumber + " Seats: " + r.numberOfSeats);
     }
